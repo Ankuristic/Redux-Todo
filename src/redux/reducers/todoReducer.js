375077fe5e4468b3d66461c59bbfc415ccea846d
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2023-10-13 00:55:18 +0530

    first commit

---
+++ src/redux/reducers/todoReducer.js
@@ -0,0 +1,37 @@
+import { ADD_TODO,TOGGLE_TODO } from "../actions/action";
+
+const initialstate ={
+    todo:[]
+}
+
+export function todoReducer(state=initialstate, action){
+
+    switch(action.type){
+        case ADD_TODO:
+            return {
+                ...state,
+                todos:[
+                    ...state.todos,
+                    {
+                        text:action.text,
+                        completed:false
+
+                    }
+                ]
+            }
+
+            case TOGGLE_TODO:
+            return {
+                ...state,
+                todos:state.todos.map((todo,i)=>{
+                    if(i==action.index){
+                        todo.completed=!todo.completed
+                    }
+                    return todo;
+                })
+
+            }
+            default :
+             return state
+    }
+}
